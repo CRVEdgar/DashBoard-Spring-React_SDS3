4ca0aab631f17662751cd69f28bdee27a00db0bf
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-05-08 15:34:17 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum = {
-    sellerName: string,
+    sellerName: string;
     sum: number
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
